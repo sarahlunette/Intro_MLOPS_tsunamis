e515a4b863bd033693a2b75f99663ed13585d396
Sarah Le Net <sarahlunette@users.noreply.github.com>
2024-07-18 15:51:24 +0200

    fastapi

--- apps/main_functional_2.py
+++ apps/main_functional_2.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from predict import predict_record
+from pydantic import BaseModel
+
 
 api = FastAPI()
 
@@ -8,10 +10,10 @@ import pickle as pkl
 
 model = pkl.load(open('model/model.pkl', 'rb'))
 
-Class User_Input(BaseModel):
+class InputData(BaseModel):
   month : int
   day : int
-  country : int
+  country : str
   period : int
   latitude : float
   longitude : float
@@ -69,12 +71,50 @@ columns = [
  'map_eq_id',
 ]
 
+columns_final = ['month', 'day', 'period', 'latitude', 'longitude', 'runup_ht',
+       'runup_ht_r', 'runup_hori', 'dist_from_', 'hour', 'cause_code',
+       'event_vali', 'eq_mag_unk', 'eq_mag_mb', 'eq_mag_ms', 'eq_mag_mw',
+       'eq_mag_mfa', 'eq_magnitu', 'eq_magni_1', 'eq_depth', 'max_event_',
+       'ts_mt_ii', 'ts_intensi', 'num_runup', 'num_slides', 'map_slide_',
+       'map_eq_id', 'country_bangladesh', 'country_canada', 'country_chile',
+       'country_china', 'country_dominican republic', 'country_egypt',
+       'country_el salvador', 'country_fiji', 'country_greece',
+       'country_haiti', 'country_india', 'country_indonesia',
+       'country_jamaica', 'country_japan', 'country_kenya',
+       'country_madagascar', 'country_malaysia', 'country_maldives',
+       'country_mexico', 'country_myanmar', 'country_new caledonia',
+       'country_nicaragua', 'country_pakistan', 'country_papua new guinea',
+       'country_peru', 'country_philippines', 'country_samoa',
+       'country_solomon islands', 'country_somalia', 'country_sri lanka',
+       'country_taiwan', 'country_tanzania', 'country_tonga', 'country_turkey',
+       'country_united kingdom', 'country_united states', 'country_vanuatu',
+       'country_venezuela', 'country_yemen']
+
 @api.post('/predict/')
-async def predict(input:User_Input):
-  # Load JSON data
-  data = json.loads(input)
-  # Convert the entire record to a pandas Series
-  record = pd.Series(data)
+async def predict(input_data:InputData):
+
+  # Change the country name key
+  country = 'country_' + input_data.country
+  data = input_data.dict()
+  data[country] = data[input_data.country]
+  del data[input_data.country]
+
+  ## Convert the entire record to a pandas Series
+  # Change the values to lists of values
+  for key in data.keys():
+    data[key] = [data[key]] # You have to remove country from this
+
+  ## create the dataframe for predictions
+  record_predict = pd.DataFrame(columns = columns_final, index = [0])
+  # Fill the values with the user input
+  record_predict[record.columns] = record
+  # Fill the rest with 0 because they are not in these countries
+  record_predict.fillna(0, inplace = True) # That is correct only if the other values are nan
+
+  ## TO-DO: Add KMeans for filling clustering column and the rest
+
+  print(record)
+  print(record.shape)
   result = predict_record(record, model)
   return {"prediction": str(result)}
   
--- src/scripts/run.py
+++ src/scripts/run.py
@@ -16,9 +16,9 @@ def add_noise(X, noise_level=0.001):
     return X + noise
 
 def predict_tsne(df, i, k):
-  tsne = TSNE(n_components=2,perplexity=i, random_state=42)
+  tsne = TSNE(n_components=2,perplexity=i + 1, random_state=42)
   X_tsne = tsne.fit_transform(df)
-  km = KMeans(n_clusters= k)
+  km = KMeans(n_clusters= k + 1)
   centroids = km.fit(X_tsne)
   df['clustering'] = pd.Series(km.labels_)
   X = df
@@ -46,15 +46,16 @@ human_damages, houses_damages = preprocess() #rassembler les deux preprocessing 
 human_damages_scaled, houses_damages_scaled = scale(human_damages, houses_damages)
 
 score = pd.DataFrame(columns = range(80), index = range(90))
-for k in range(1, 90):
-  for i in range(1, 80):
+for k in range(90):
+  for i in range(80):
     gbr,r2 = predict_tsne(human_damages_scaled, i, k)
-    score.iloc[k-1, i-1] = r2
+    score.iloc[k, i] = r2
 
-best_n = np.argmax(np.array(score.values))
-print('Best number of clusters', best_n, 'Best score', score.iloc[best_n[0], best_n[1]])
-
-gbr, r2 = predict_tsne(human_damages_scaled, best_n[1] + 1 , best_n[0] + 1)
-
-print(r2)
-print(best_n)
+  print(score)
+  best_n = np.argmax(np.array(score.values))
+  print(best_n)
+  max_score = score.max
+  print(score.max)
+  max_value = df.idxmax().max()  # This will give you the index of the maximum value in the DataFrame
+  max_column_name = df.idxmax().idxmax()  # This will give you the column name of the maximum value
+  print('perplexity ' + str(max_column_name), 'n_neighbors ' + str(max_value))
