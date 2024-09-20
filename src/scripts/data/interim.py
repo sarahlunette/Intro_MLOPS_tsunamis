10a07bfc3ec04dff845f790f25e54868c6a13d86
Sarah Le Net <sarahlunette@users.noreply.github.com>
2024-09-20 15:40:06 +0200

    Create interim.py

---
+++ src/scripts/data/interim.py
@@ -0,0 +1,13 @@
+from create_db import insert_data_from_csv, create_connection
+
+''' TODO: replace with the right credentials for the interim db'''
+db_config = {
+    "host": "your_host",
+    "database": "your_dbname",
+    "user": "your_username",
+    "password": "your_password",
+    "port": "your_port"
+}
+
+conn = create_connection(db_config)
+insert_data_from_csv(conn, csv_file)
